d9e46de6f3913820b0da971968bb3aaa5085d30e
James Hong <hongjn@cs.stanford.edu>
2018-10-09 19:15:13 -0700

    Working indexer

--- build_index.py
+++ build_index.py
@@ -3,23 +3,39 @@
 import argparse
 import os
 import pysrt
+import heapq
 from tqdm import tqdm
 from threading import Lock
 from multiprocessing import Pool
-from collections import defaultdict
+from collections import defaultdict, namedtuple, Counter
 
-from util.index import DocumentIndex, tokenize, UNKNOWN_TOKEN_ID
+from util.index import tokenize, Lexicon, Documents
 
 
 N_WORKERS = None
 
+ENDIAN = 'little'
+DATUM_SIZE = 4
+MAX_INT = 2 ** (DATUM_SIZE * 8) - 1
+
+
+def encode_datum(i):
+    assert isinstance(i, int)
+    assert i >= 0 and i <= MAX_INT, 'Out of range: {}'.format(i)
+    return (i).to_bytes(DATUM_SIZE, ENDIAN)
+
+
+def decode_datum(s):
+    assert len(s) == DATUM_SIZE, '{} is too short'.format(len(s))
+    return int.from_bytes(s, ENDIAN)
+
 
 def get_args():
     p = argparse.ArgumentParser()
-    p.add_argument('subdir', type=str)
-    p.add_argument('-o', dest='outpath', type=str, default='index.bin')
-    p.add_argument('-j', dest='workers', type=int, default=8)
-    p.add_argument('--debug', dest='debug', action='store_true')
+    p.add_argument('doc_dir', type=str)
+    p.add_argument('-o', dest='out_dir', type=str, default='build')
+    p.add_argument('-j', dest='workers', type=int, default=os.cpu_count())
+    p.add_argument('--limit', dest='limit', type=int)
     return p.parse_args()
 
 
@@ -27,120 +43,327 @@ def list_subs(dir):
     return [f for f in os.listdir(dir) if f.endswith('.srt')]
 
 
-def load_srt(docpath):
+def load_srt(doc_path):
     try:
-        subs = pysrt.open(docpath)
+        subs = pysrt.open(doc_path)
     except:
         try:
-            subs = pysrt.open(docpath, encoding='iso-8859-1')
+            subs = pysrt.open(doc_path, encoding='iso-8859-1')
         except:
-            raise Exception(f'Cannot parse {docpath}')
+            raise Exception('Cannot parse {}'.format(doc_path))
     return subs
 
 
-def get_doc_words(docpath):
+def get_doc_words(doc_path):
     try:
-        subs = load_srt(docpath)
+        subs = load_srt(doc_path)
     except Exception as e:
         print(e)
         return set()
 
-    words = set()
+    words = Counter()
     for s in subs:
         tokens = tokenize(s.text)
         words.update(tokens)
     return words
 
 
-def get_words(subdir, subfiles):
-    words = set()
-    wordsLock = Lock()
-    with tqdm(total=len(subfiles)) as pbar:
+def get_words(doc_dir, docs):
+    assert isinstance(doc_dir, str)
+    assert isinstance(docs, Documents)
+
+    words = Counter()
+    words_lock = Lock()
+    with tqdm(total=len(docs)) as pbar, Pool(processes=N_WORKERS) as pool:
         pbar.set_description('Building lexicon')
 
         def collect(result):
-            with wordsLock:
+            with words_lock:
                 words.update(result)
             pbar.update(1)
 
-        pool = Pool(processes=N_WORKERS)
-
-        for subfile in subfiles:
-            docpath = os.path.join(subdir, subfile)
-            pool.apply_async(get_doc_words, (docpath,), callback=collect)
+        for d in docs:
+            doc_path = os.path.join(doc_dir, d)
+            if N_WORKERS > 1:
+                pool.apply_async(get_doc_words, (doc_path,), callback=collect)
+            else:
+                collect(get_doc_words(doc_path))
 
         pool.close()
         pool.join()
 
-    print(f'Lexicon size: {len(words)}')
+    print('Lexicon size: {}'.format(len(words)))
     return words
 
 
-def get_doc_index(sub_id, docpath, rlexicon):
-    """
-    Returns: {
-        token : [
-            (position, startTime, endTime), ...
-        ]
-    }
-    """
+def reverse_index_single_doc(doc_path, lexicon):
+    assert isinstance(lexicon, Lexicon)
+
+    doc_rindex = defaultdict(list)
     try:
-        subs = load_srt(docpath)
+        subs = load_srt(doc_path)
+        doc_position = 0
+        for s in subs:
+            start, end = s.start.ordinal, s.end.ordinal
+            for t in tokenize(s.text):
+                token = lexicon[t]
+                if token is None:
+                    print('Unknown token: {}'.format(t))
+                else:
+                    doc_rindex[token.id].append((doc_position, start, end))
+                doc_position += 1
     except Exception as e:
         print(e)
-        return {}
-    doc_index = defaultdict(list)
-    position = 0
-    for s in subs:
-        start, end = s.start.ordinal / 1000., s.end.ordinal / 1000.
-        for t in tokenize(s.text):
-            tid = rlexicon.get(t, UNKNOWN_TOKEN_ID)
-            doc_index[tid].append((position, start, end))
-            position += 1
-    return sub_id, doc_index
+    return doc_rindex
 
 
-def build_index(subdir, subfiles, lexicon):
-    # {
-    #     token: { doc_id: "list of occurrences" },
-    # }
-    rlexicon = { w : i for i, w in enumerate(lexicon) }
-    indexLock = Lock()
-    index = {}
-    with tqdm(total=len(subfiles)) as pbar:
-        pbar.set_description('Building index')
+# Hack to get around sharing args beween processes workers
+WORKER_LEXICON = None
 
-        def collect(result):
-            with indexLock:
-                sub_id, doc_index = result
-                for t, l in doc_index.items():
-                    if t not in index:
-                        index[t] = {}
-                    index[t][sub_id] = l
-            pbar.update(1)
 
-        pool = Pool(processes=N_WORKERS)
+def reverse_index_batch(doc_batch, out_path):
+    lexicon = WORKER_LEXICON
 
-        for i, subfile in enumerate(subfiles):
-            docpath = os.path.join(subdir, subfile)
-            pool.apply_async(get_doc_index, (i, docpath, rlexicon), callback=collect)
+    assert isinstance(out_path, str)
+    assert isinstance(lexicon, Lexicon)
+
+    batch_rindex = defaultdict(list)     # token -> [(doc, [postings])]
+    for doc_id, doc_path in doc_batch:
+        doc_index = reverse_index_single_doc(doc_path, lexicon)
+        for token_id, postings in doc_index.items():
+            batch_rindex[token_id].append((doc_id, postings))
+
+    with open(out_path, 'wb') as f:
+        # Order by increasing token_id
+        for token_id in sorted(batch_rindex):
+            docs = batch_rindex[token_id]
+            assert len(docs) > 0
+
+            f.write(encode_datum(token_id))    # Token id
+            f.write(encode_datum(len(docs)))   # Num of docs in list
+
+            # Order by increasing doc_id
+            for doc_id, postings in sorted(docs, key=lambda x: x[0]):
+                f.write(encode_datum(doc_id))          # Doc id
+                f.write(encode_datum(len(postings)))   # Num postings in doc
+                assert len(postings) > 0
+
+                for (position, start, end) in postings:
+                    f.write(encode_datum(position))    # Position in document
+                    f.write(encode_datum(start))       # Start time in ms
+                    f.write(encode_datum(end))         # End time in ms
+
+    return len(doc_batch)
+
+
+def reverse_index_all_docs(doc_dir, docs, lexicon, out_dir, batch_size=100):
+    assert isinstance(docs, Documents)
+    assert isinstance(lexicon, Lexicon)
+
+    global WORKER_LEXICON
+    WORKER_LEXICON = lexicon
+
+    with tqdm(total=len(docs)) as pbar, Pool(processes=N_WORKERS) as pool:
+        async_results = []
+        pbar.set_description('Building inverted index')
+
+        def progress(n_indexed):
+            pbar.update(n_indexed)
+
+        for base_id in range(0, len(docs), batch_size):
+            doc_batch = [
+                (i, os.path.join(doc_dir, docs[i]))
+                for i in range(base_id, base_id + batch_size)
+            ]
+            out_path = os.path.join(out_dir, '{}.bin'.format(base_id))
+
+            if N_WORKERS > 1:
+                async_results.append(pool.apply_async(
+                    reverse_index_batch,
+                    (doc_batch, out_path),
+                    callback=progress))
+            else:
+                progress(reverse_index_batch(doc_batch, out_path))
+
+        for async in async_results:
+            async.wait()
+            assert async.successful()
 
         pool.close()
         pool.join()
-    return index
 
 
-def main(subdir, outpath, workers, debug):
+class MergeIndexParser(object):
+
+    Document = namedtuple('Document', ['id', 'n', 'data'])
+
+    def __init__(self, path):
+        self._path = path
+        self._f = open(path, 'rb')
+        self._eof = False
+        self._curr_token = None
+        self._curr_doc = None
+        self._curr_n_docs = None
+        self._curr_n_docs_left = 0
+        self.next_token()
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def token(self):
+        return self._curr_token if not self._eof else None
+
+    @property
+    def ndocs(self):
+        assert not self._eof, 'EOF already read'
+        return self._curr_n_docs
+
+    @property
+    def doc(self):
+        assert not self._eof, 'EOF already read'
+        return self._curr_doc
+
+    def next_token(self):
+        assert not self._eof, 'EOF already read'
+        assert self._curr_n_docs_left == 0, 'Not done processing docs'
+        token_data = self._f.read(DATUM_SIZE)
+        if len(token_data) == 0:
+            self._f.close()
+            self._eof = True
+            self._curr_token = None
+            self._curr_n_docs = None
+            self._curr_n_docs_left = None
+        else:
+            ndocs_data = self._f.read(DATUM_SIZE)
+            self._curr_token = decode_datum(token_data)
+            self._curr_n_docs = decode_datum(ndocs_data)
+            self._curr_n_docs_left = self._curr_n_docs
+            assert self._curr_n_docs > 0, 'Token cannot have no docs'
+            assert self.next_doc() is not None
+        return self.token
+
+    def next_doc(self):
+        assert self._curr_n_docs_left >= 0
+        if self._curr_n_docs_left == 0:
+            self._curr_doc = None
+        else:
+            self._curr_n_docs_left -= 1
+            doc_id = decode_datum(self._f.read(DATUM_SIZE))
+            n_postings = decode_datum(self._f.read(DATUM_SIZE))
+            assert n_postings > 0, 'Empty postings list'
+            postings_len = n_postings * DATUM_SIZE * 3  # (position, start, end)
+            postings_data = self._f.read(postings_len)
+            assert len(postings_data) == postings_len, 'Invalid read'
+            self._curr_doc = MergeIndexParser.Document(
+                id=doc_id, n=n_postings, data=postings_data)
+        return self.doc
+
+    def __lt__(self, o):
+        # For priority queuing
+        if self.token == o.token:
+            return self.doc < o.doc
+        return self.token == o.token
+
+
+def merge_reverse_indexes(ridx_dir, lexicon, out_path):
+    assert isinstance(lexicon, Lexicon)
+
+    ridx_paths = [
+        os.path.join(ridx_dir, x)
+        for x in os.listdir(ridx_dir) if x.endswith('.bin')
+    ]
+    token_parsers_pq = []
+    for path in ridx_paths:
+        heapq.heappush(token_parsers_pq, MergeIndexParser(path))
+
+    jump_offsets = [-1] * len(lexicon)
+    with open(out_path, 'wb') as f, tqdm(total=len(lexicon)) as pbar:
+        for i in range(len(lexicon)):
+            if len(token_parsers_pq) == 0:
+                break
+            token_id = token_parsers_pq[0].token
+            pbar.set_description(
+                'Merging indexes ("{}")'.format(lexicon[token_id].token))
+            jump_offsets[token_id] = f.tell()
+
+            doc_parsers_pq = []
+            doc_count = 0
+            while True:
+                if (len(token_parsers_pq) == 0 or
+                        token_parsers_pq[0].token != token_id):
+                    break
+                p = heapq.heappop(token_parsers_pq)
+                doc_count += p.ndocs
+                heapq.heappush(doc_parsers_pq, p)
+            assert len(doc_parsers_pq) >= 1
+
+            f.write(encode_datum(token_id))   # Token id
+            f.write(encode_datum(doc_count))  # Num docs
+
+            while len(doc_parsers_pq) > 0:
+                p = heapq.heappop(doc_parsers_pq)
+                f.write(encode_datum(p.doc.id))      # Doc id
+                f.write(encode_datum(p.doc.n))       # Num postings
+                f.write(p.doc.data)                  # Raw postings data
+
+                if p.next_doc() is None:
+                    if p.next_token() is not None:
+                        # Return to tokens queue
+                        heapq.heappush(token_parsers_pq, p)
+                else:
+                    # Return to docs queue
+                    heapq.heappush(doc_parsers_pq, p)
+
+    return jump_offsets
+
+
+def main(doc_dir, out_dir, workers, limit=None):
     global N_WORKERS
     N_WORKERS = workers
 
-    subfiles = list(sorted(list_subs(subdir)))
-    if debug:
-        subfiles = subfiles[:100]
+    docs = list(sorted(list_subs(doc_dir)))
+    if limit is not None:
+        docs = docs[:limit]
+    docs = Documents(docs)
 
-    lexicon = list(sorted(get_words(subdir, subfiles)))
-    index = build_index(subdir, subfiles, lexicon)
-    DocumentIndex(index=index, doclist=subfiles, lexicon=lexicon).store(outpath)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    lex_path = os.path.join(out_dir, 'words.lex')
+    if os.path.exists(lex_path):
+        print('Loading lexicon: {}'.format(lex_path))
+        lexicon = Lexicon.load(lex_path)
+    else:
+        # Save lexicon without offsets initally
+        wordcounts = get_words(doc_dir, docs)
+        lexicon = Lexicon([
+            Lexicon.Word(i, w, wordcounts[w], -1)
+            for i, w in enumerate(sorted(wordcounts.keys()))
+        ])
+        lexicon.store(lex_path)
+        del wordcounts
+
+    doc_list_path = os.path.join(out_dir, 'documents.txt')
+    docs.store(doc_list_path)
+
+    chunk_dir = os.path.join(out_dir, 'parts')
+    if not os.path.exists(chunk_dir):
+        os.makedirs(chunk_dir)
+        reverse_index_all_docs(doc_dir, docs, lexicon, chunk_dir)
+    else:
+        print('Exists: {}'.format(chunk_dir))
+
+    idx_path = os.path.join(out_dir, 'index.bin')
+    jump_offsets = merge_reverse_indexes(chunk_dir, lexicon, idx_path)
+
+    # Resave the lexicon with offsets
+    lexicon = Lexicon([
+        Lexicon.Word(w.id, w.token, w.count, jump_offsets[w.id])
+        for w in lexicon
+    ])
+    lexicon.store(lex_path)
 
 
 if __name__ == '__main__':
--- util/index.py
+++ util/index.py
@@ -1,33 +1,89 @@
-import _pickle as pickle
 import nltk
-
-
-UNKNOWN_TOKEN_ID = -1
+from collections import namedtuple
 
 
 def tokenize(s):
     return nltk.word_tokenize(s)
 
 
-class DocumentIndex(object):
+class Lexicon(object):
 
-    def __init__(self, index, doclist, lexicon):
-        self._index = index
-        self._doclist = doclist
-        self._lexicon = lexicon
+    Word = namedtuple('Word', ['id', 'token', 'count', 'offset'])
 
-    def search(self, s):
-        pass
+    def __init__(self, words):
+        assert isinstance(words, list)
+        self._words = words
+        self._inverse = {w.token: w for i, w in enumerate(self._words)}
+
+    def __iter__(self):
+        return self._words.__iter__()
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self._words[key]
+        elif isinstance(key, str):
+            return self._inverse.get(key, None)
+        raise KeyError()
+
+    def __contains__(self, key):
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return False
+        return True
+
+    def __len__(self):
+        return len(self._words)
 
     def store(self, path):
-        with open(path, 'wb') as f:
-            pickle.dump({
-                'index': self._index,
-                'doclist': self._doclist,
-                'lexicon': self._lexicon
-            }, f, protocol=-1)
+        with open(path, 'w') as f:
+            for w in self._words:
+                f.write('{}\t{}\t{}\n'.format(w.token, w.count, w.offset))
 
     @staticmethod
     def load(path):
-        with open(path, 'rb') as f:
-            return DocumentIndex(**pickle.load(f))
+        words = []
+        with open(path, 'r') as f:
+            for i, line in enumerate(f):
+                token, count, offset = line.strip().split('\t')
+                count = int(count)
+                offset = int(offset)
+                words.append(Lexicon.Word(i, token, count, offset))
+        return Lexicon(words)
+
+
+class Documents(object):
+
+    def __init__(self, docs):
+        self._docs = docs
+
+    def __iter__(self):
+        return self._docs.__iter__()
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self._docs[key]
+        elif isinstance(key, str):
+            return self._docs.index(key)
+        raise KeyError()
+
+    def __contains__(self, key):
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return False
+        return True
+
+    def __len__(self):
+        return len(self._docs)
+
+    def store(self, path):
+        with open(path, 'w') as f:
+            for d in self._docs:
+                f.write(d)
+                f.write('\n')
+
+    @staticmethod
+    def load(path):
+        with open(path, 'r') as f:
+            return Documents([d.strip() for d in f])
