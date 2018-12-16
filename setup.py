abb9d4e4ee044068c49fc4f8c63f71fa89cec7ac
Will Crichton <wcrichto@cs.stanford.edu>
2018-12-16 03:20:16 +0000

    Simplify setup

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
-
-class PyTest(TestCommand):
-    user_options = []
-
-    def run(self):
-        self.run_command("test_rust")
-
-        import subprocess
-
-        subprocess.check_call(["pytest", "tests"])
-
-
-setup_requires = []
-install_requires = ['msgpack', 'numpy', 'pysrt', 'spacy', 'tqdm']
+setup_requires = ['pytest-runner']
+install_requires = [
+    'msgpack==0.5.6',
+    'numpy>=1.15.4',
+    'pysrt>=1.1.1',
+    'pytest>=4.0.1',
+    'spacy>=2.0.18',
+    'tqdm>=4.28.1'
+]
 tests_require = install_requires + ["pytest"]
 
 setup(
@@ -26,6 +20,5 @@ setup(
     tests_require=tests_require,
     setup_requires=setup_requires,
     include_package_data=True,
-    zip_safe=False,
-    cmdclass=dict(test=PyTest),
+    zip_safe=False
 )
